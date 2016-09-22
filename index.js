9f125bec58eada666c75582ef4e8b75420d32c47
Daniel McQuiston <daniel.pro@dehugo.net>
2016-09-22 13:38:44 -0600

    Retrieve input file via passed param

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 "use strict"
 
-
+const inputFile = process.argv[2]
 
 const crypto = require("crypto")
 const fs = require("fs")
+const path = require("path")
 const mmm = require("mmmagic")
 const Magic = mmm.Magic
 const ExifImage = require("exif").ExifImage
 const musicmetadata = require("musicmetadata")
 const jsmediatags = require("jsmediatags")
+const metaflac = require("metaflac")
 
 const magic = new Magic(mmm.MAGIC_MIME_TYPE)
+
+const FILE_NAME = path.resolve(inputFile)
 
 new Promise((resolve, reject)=>{
 	fs.readFile(FILE_NAME, (fsErr, fileBuffer)=>{
@@ -37,6 +41,16 @@ new Promise((resolve, reject)=>{
 				} catch (unkExifErr) {
 					reject(unkExifErr)
 				}
+			} else if (fileType === "audio/x-flac") {
+				metaflac.list([["exceptBlockType","SEEKTABLE"],["exceptBlockType","PICTURE"]], FILE_NAME, (flacErr, metadataBlocks)=>{
+					if (flacErr) return resolve(fileData)
+
+					metaflac.showMD5sum({}, FILE_NAME, (flacMd5Err, flacMd5sum)=>{
+						if (flacMd5Err) return resolve(fileData)
+
+						resolve(Object.assign({}, fileData, {metaFlacMd5: flacMd5sum, metaFlac: metadataBlocks}))
+					})
+				})
 			} else if (fileType.split("/")[0] === "audio") {
 				/*musicmetadata(fs.createReadStream(FILE_NAME), (id3Err, metadata)=>{
 					if (id3Err) return resolve(fileData)
