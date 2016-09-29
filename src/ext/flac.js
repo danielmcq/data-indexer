2c5f63d41868b06297db7293c5b8864a0ec569c7
Daniel McQuiston <daniel.pro@dehugo.net>
2016-09-29 13:40:15 -0600

    Bypass flac module

--- src/ext/flac.js
+++ src/ext/flac.js
@@ -5,16 +5,17 @@ const metaflac = require("metaflac")
 exports.isFlac = file => file.type === "audio/x-flac"
 
 exports.parse = file => new Promise((resolve, reject)=>{
-	metaflac.list([["exceptBlockType","SEEKTABLE"],["exceptBlockType","PICTURE"]], file.path, (flacErr, metadataBlocks)=>{
+	// Throwing error
+	/*metaflac.list([["exceptBlockType","SEEKTABLE"],["exceptBlockType","PICTURE"]], file.path, (flacErr, metadataBlocks)=>{
 		if (flacErr) return resolve(file)
 
-		/*metaflac.showMD5sum({}, file.path, (flacMd5Err, flacMd5sum)=>{
+		metaflac.showMD5sum({}, file.path, (flacMd5Err, flacMd5sum)=>{
 			if (flacMd5Err) return resolve(file)
 
 			Object.assign(file.meta, {metaFlacMd5: flacMd5sum, metaFlac: metadataBlocks})
 
 			resolve(file)
-		})*/
-resolve(file)
-	})
+		})
+	})*/
+	resolve(file)
 })
